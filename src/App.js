f7cea605ae55cb6c0be4e47a676ec38a91e66463
Sanabel8 <sanabeljamal97@gmail.com>
2021-06-15 14:04:44 +0300

    lab7

--- src/App.js
+++ src/App.js
@@ -18,9 +18,10 @@ export class App extends React.Component {
     this.state = {
       cityName: '',
       cityData: {},
-      displayDta: false,
+      displayData: false,
       alert: false,
-      error: ''
+      error: '',
+      weatherData: []
     }
 
   };
@@ -35,33 +36,42 @@ export class App extends React.Component {
     e.preventDefault();
 
 
-    try{
+    try {
       const axiosResponse = await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.cf35e1cf53a07585c594d6c5e38401db&q=${this.state.cityName}&format=json`);
       console.log(axiosResponse.data[0]);
+
+      const weatherDataRes = await axios.get(`http://localhost:8080/weather`);
+      console.log(weatherDataRes.data.data);
+
       this.setState({
         cityData: axiosResponse.data[0],
         displayDta: true,
-        alert : false
+        alert: false,
+        weatherData: weatherDataRes.data.data
       })
-  
+
     } catch (error) {
       this.setState({
         error: error.message,
-        alert :true
+        alert: true,
+        // displayDta: false,
+
       })
     }
-   
+
   }
 
 
   //https://us1.locationiq.com/v1/search.php?key=pk.cf35e1cf53a07585c594d6c5e38401db&q=amman&format=json
+
+  // http://localhost:8080/weather
   render() {
     return (
       <div>
         {this.state.alert &&
-        <Alert variant="danger">
-        This is a {this.state.error} alert—check it out!
-      </Alert>
+          <Alert variant="danger">
+            This is a {this.state.error} alert—check it out!
+          </Alert>
         }
         <Form onSubmit={this.getData}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -72,7 +82,8 @@ export class App extends React.Component {
             explore!
           </Button>
         </Form>
-        {this.state.displayDta &&
+
+        {this.state.displayData &&
 
 
           <div>
@@ -80,8 +91,7 @@ export class App extends React.Component {
               {this.state.cityData.display_name}
             </p>
             <p>`The lat : {this.state.cityData.lat}`</p>
-                  <p>`The lon : {this.state.cityData.lon}`</p>
-
+            <p>`The lon : {this.state.cityData.lon}`</p>
 
 
             <Container>
@@ -90,14 +100,21 @@ export class App extends React.Component {
                 <Col xs={6} md={4}>
                   <Image src={`https://maps.locationiq.com/v3/staticmap?key=pk.cf35e1cf53a07585c594d6c5e38401db&q&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=15`} alt='' roundedCircle />
 
-                  
-                 
+
+
                 </Col>
 
 
               </Row>
             </Container>
-
+            <div>
+              {
+                this.state.displayData &&
+                this.state.weatherData.map(value =>{
+                  return (<p>{value.weather.description}</p>)
+                })
+                 }
+            </div>
 
 
           </div>
@@ -112,3 +129,6 @@ export class App extends React.Component {
   }
 }
 export default App;
+
+
+
